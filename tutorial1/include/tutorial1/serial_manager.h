57a9b1ffcccafde68a269f2f3ed1c787238c7be3
yuhangc <cccumg@gmail.com>
2017-05-08 17:05:50 -0700

    added code skeletons for tutorial1

---
+++ tutorial1/include/tutorial1/serial_manager.h
@@ -0,0 +1,28 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "std_msgs/Float32MultiArray.h"
+
+#include "CArduinoDevice.h"
+
+class SerialManager
+{
+public:
+    // constructor
+    SerialManager(ros::NodeHandle &nh, ros::NodeHandle &pnh);
+
+    // destructor
+    ~SerialManager();
+
+    // main update function
+    void update();
+
+private:
+    // node handler
+    ros::NodeHandle nh_;
+
+    // subscriber and publisher
+    ros::Publisher serial_data_pub;
+
+    // arduino device
+    CArduinoDevice* arduino;
+};
